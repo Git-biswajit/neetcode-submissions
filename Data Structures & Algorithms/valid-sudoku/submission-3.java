7edf3a68fb97b487573cf22d2935dd159cb4074a
Biswajit Swain <73901428+Git-biswajit@users.noreply.github.com>
2026-08-30 17:59:21 +0530

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> set = new HashSet<>();
+        int n = board.length;
+        int m = board[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                char ch = board[i][j];
+                if(ch=='.'){
+                    continue;
+                }
+                String row = ch + "row" +i;
+                String col = ch + "col" +j;
+                String box = ch + "box" +(i/3)+(j/3);
+                if(set.contains(row)|| set.contains(col) || set.contains(box)){
+                    return false;
+                }
+                set.add(row);
+                set.add(col);
+                set.add(box);
+
+            }
+        }
+        return true;
+        
+    }
+}
